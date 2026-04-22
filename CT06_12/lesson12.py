580af9b2aeae2d41c4735b6849d4f267736b3548
l123-cpu <lucasng978@gmail.com>
2026-04-22 07:34:55 +0000

    11

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,15 +1,67 @@
-order=""
+# order=""
 
-a=input("what are you ordering")
-while a !="end":
-    print(a)
-a=input("what are you ordering")
-while a !="end":
-    print(a)
-a=input("what are you ordering")
-while a !="end":
-    print(a)
-a=input("what are you ordering")
-while a !="end":
-    print(a)
-print(a)
+# for i in range (10):
+#     a=input("what are you ordering")
+#     while a !="end":
+#         print(a)
+#     break
+
+
+
+# n1=int(input("what is the number?: "))
+
+# if n1%3==0 and n1%5==0:
+#   print("yes it is divisable by 3 and 5")
+# else:
+#   print("is isnt divisable by 3 and/or 5")
+
+
+# v=int(input("how many visitors are there"))
+# mv=int(input("max visitor?"))
+
+# while True:
+#     if v<mv:
+#         v+=1
+#         print(v)
+#     elif v>=mv:
+#         break
+
+
+
+
+# order = ""
+
+# while True:
+#     user_input = input("Enter your order (type 'end' to finish): ")
+    
+#     if user_input == "end":
+#         break
+    
+#     order += user_input + " "
+
+
+# print("Customer's order:", order)
+
+
+
+# count = 10
+
+# while count >= 1:
+#     print(count)
+#     count -= 1
+#     if count==5:
+#         break
+# else:
+#     print("Happy New Year!")
+
+
+
+import random
+while True:
+    a=random.randint(1,10)
+    b=random.randint(1,10)
+    quesans=int(input("what is"+str(a)+"+"+str(b)))
+    if quesans!=(a+b):
+        print("wrong")
+    else:
+        print("correct")
--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,47 @@
-print("Hello from lesson 13")
+# import random
+# n=0
+# while n!=4:
+#     n=random.randint(1,6)
+#     print(n)
+
+# bal=1000
+
+# while True:
+#     ans=input("Would you like to withdraw, deposit, check your balance? Type end to exit")
+#     if ans=="withdraw":
+#         withmon=int(input("how much would you like to withdraw?"))
+#         if withmon>bal:
+#             print("you cannot withdraw more than u have.")
+#         elif bal>withmon:
+#             bal=bal-withmon
+#             print("your balance is now "+ str(bal))
+#     if ans=="deposit":
+#         inmon=int(input("how much would u like to deposit?"))
+#         bal=bal+inmon
+#         print("your balance is now "+ str(bal))
+#     if ans=="check my balance":
+#         print (bal)
+#     if ans=="end":
+#         break
+g=["apples", "carrots", "grape"]
+
+# for i in range(4):
+#     if g=="apples":
+#             print("Apple: i need 5 of these")
+#     if g=="carrots":
+#             print("Apple: i need 3 of these")
+#     if g=="grapes":
+#             print("grapes:get the farmfresh brand")
+s=[]   
+while True:
+    t=input("what have u bought")
+    if u =="end":
+        break
+    t.append(u)
+
+hello lucas
+i am definitely not a hacker lolllllll
+EZZZZZZZZZZZZZ
+LLLLL
+ur code so cooked ez
+get hacked lil Bro
